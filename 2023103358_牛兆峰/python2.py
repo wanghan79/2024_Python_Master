adf35dbe9a63be360fc00ebfda9c47357a57ad5b
Nancyxiaonan <157865162+Nancyxiaonan@users.noreply.github.com>
2024-06-14 10:59:28 +0800

    Add files via upload

---
+++ 2023103358_牛兆峰/python2.py
@@ -0,0 +1,37 @@
+import random
+import string
+
+class DataSampler:
+    def __init__(self, num, struct):
+        self.num = num
+        self.struct = struct
+
+    def structDataSampling(self):
+        results = list()
+        for _ in range(self.num):
+            result = list()
+            for key, value in self.struct.items():
+                if key is int:
+                    it = iter(value['datarange'])
+                    tmp = random.randint(next(it), next(it))
+                elif key is float:
+                    it = iter(value['datarange'])
+                    tmp = random.uniform(next(it), next(it))
+                elif key is str:
+                    tmp = ''.join(random.SystemRandom().choice(value['datarange']) for _ in range(value['len']))
+                else:
+                    continue  # Skip unknown types
+                result.append(tmp)
+            results.append(result)
+        return results
+
+    def displaySamples(self):
+        samples = self.structDataSampling()
+        for sample in samples:
+            print(sample)
+
+if __name__ == '__main__':
+    sampler = DataSampler(num=5, struct={int: {'datarange': (0, 10)},
+                                         str: {'datarange': string.ascii_letters + "&*_", 'len': 10},
+                                         float: {'datarange': (0, 1.0)}})
+    sampler.displaySamples()
