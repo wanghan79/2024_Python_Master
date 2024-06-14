78d173ebe5b85317f234ec12ea9f02afad3a5f1e
15689373909 <50036398+15689373909@users.noreply.github.com>
2024-06-14 10:43:15 +0800

    Update homework4.py

--- 20231013393_朱厚峰/homework4.py
+++ 20231013393_朱厚峰/homework4.py
@@ -6,9 +6,9 @@ import homework3
 def run():
     while True:
         print("请按需求输入数字进行对应模块的测试：")
-        print('输入1：执行homework1：执行封装函数的调用示例')
-        print('输入2：执行homework2：采用修饰器技术对作业1随机数据结构生成函数进行修饰，执行所有生成随机数的不同操作的调用示例')
-        print('输入3：执行homework3：')
+        print('输入1：执行homework1：执行随机结构生成函数的封装示例')
+        print('输入2：执行homework2：随机结构生成类封装调用示例')
+        print('输入3：执行homework3：统计方法修饰类实现')
         print('输入q：退出程序')
 
         try:
