5c9ba5757bc7d1c76c7999aa7189e4a104290ffa
Accepted <65214835+764570355@users.noreply.github.com>
2024-06-14 11:14:49 +0800

    Update homework01.py

--- 2023103364_孙思帆/homework01.py
+++ 2023103364_孙思帆/homework01.py
@@ -2,35 +2,43 @@ import random
 
 def datasampling(**kwargs):
     result = []
-    n = kwargs.get('num', -1)
-    if n == -1:
-        raise Exception('Missing Parameter num!')
-    Item = kwargs.get('struct', None)
-    if Item is None:
-        raise Exception('Missing Parameter struct!')
+    try:
+        n = kwargs['num']
+    except KeyError:
+        raise ValueError("Missing parameter 'num'!")
+    
+    if n < 0:
+        raise ValueError("Parameter 'num' must be a non-negative integer!")
+    
+    try:
+        Item = kwargs['struct']
+    except KeyError:
+        raise ValueError("Missing parameter 'struct'!")
+    
+    if not isinstance(Item, dict):
+        raise TypeError("Parameter 'struct' must be a dictionary!")
+    
     for _ in range(n):
-        element = []
+        element = {}
         for key, value in Item.items():
             if value['datatype'] == 'int':
-                it = iter(value['datarange'])
-                tmp = random.randint(next(it), next(it))
+                element[key] = random.randint(*value['datarange'])
             elif value['datatype'] == 'float':
-                it = iter(value['datarange'])
-                tmp = random.uniform(next(it), next(it))
+                element[key] = random.uniform(*value['datarange'])
             elif value['datatype'] == 'str':
-                tmp = ''.join(random.SystemRandom().choice(value['datarange']) for _ in range(value['len']))
+                element[key] = ''.join(random.SystemRandom().choice(value['datarange']) for _ in range(value['len']))
             else:
-                break
-            element.append(tmp)
+                raise ValueError(f"Unsupported datatype '{value['datatype']}' for field '{key}'")
+        
         result.append(element)
 
     return result
 
-# 测试数据
+
 struct1 = {
     'fieldname1': {
         'datatype': 'int',
-        'datarange': [1, 200]
+        'datarange': (1, 200)  
     },
     'fieldname2': {
         'datatype': 'str',
@@ -39,9 +47,10 @@ struct1 = {
     },
     'fieldname3': {
         'datatype': 'float',
-        'datarange': [0, 2]
+        'datarange': (0, 2) 
     }
- }
+}
+
 
 samples1 = datasampling(num=3, struct=struct1)
 print(samples1)
