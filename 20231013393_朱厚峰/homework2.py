b02a590d1892523e7320a186f99cfeeb49ed2a60
15689373909 <50036398+15689373909@users.noreply.github.com>
2024-06-14 10:28:52 +0800

    Update homework2.py

--- 20231013393_朱厚峰/homework2.py
+++ 20231013393_朱厚峰/homework2.py
@@ -53,14 +53,3 @@ structure = {
 
 f = datasampling(num=2, struct = structure)
 print(f)
-'''
-装饰器函数decorator接受一个函数func作为参数，并定义了一个内部函数wrapper。在wrapper函数中，首先调用被装饰函数func，并将返回的数据存储在变量data中。
-
-接下来，创建一个空字典result，并将原始数据存储在键为'origin_data'的项中。
-
-然后，遍历装饰器函数的参数data_args，对于每个选项，如果选项为'AVG'，则计算数据的平均值，并将结果存储在键为'AVG'的项中。如果选项为'SUM'，则计算数据的总和，并将结果存储在键为'SUM'的项中。
-
-最后，返回结果字典result。
-
-在datasampling函数中，根据传入的参数生成样本数据。样本数据的数量由num参数确定，数据结构由struct参数指定。根据每个字段的数据类型和范围，使用随机函数生成对应类型的数据，并将数据存储在result列表中。
-'''
