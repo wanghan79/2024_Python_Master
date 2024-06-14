e88e2e2a05d00fd6b4ce043aba82909eba61d6df
Sylvia312 <134627429+Sylvia312@users.noreply.github.com>
2024-06-14 16:23:46 +0800

    Update homework1.py

--- 2023103412史春晓/homework1.py
+++ 2023103412史春晓/homework1.py
@@ -11,11 +11,9 @@ def generate_random_value(data_type, **kwargs):
         chars = kwargs.get('str_chars', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
         return ''.join(random.choices(chars, k=length))
     elif data_type == list:
-        # 如果是列表，生成列表中的每个元素
         element_types = kwargs.get('list_elements', [int])
         return [generate_random_value(t, **kwargs) for t in element_types]
     elif data_type == dict:
-        # 如果是字典，生成字典中的每个值
         keys = kwargs.get('dict_keys', {})
         return {k: generate_random_value(v, **kwargs) for k, v in keys.items()}
     else:
@@ -47,6 +45,5 @@ data_structure = {
 }
 
 if __name__ == '__main__':
-    # 传递额外参数以控制随机值生成
     random_data = generate_data_from_structure(data_structure, int_min=10, int_max=500, float_min=0.1, float_max=50.0, str_len=12)
     print(random_data)
