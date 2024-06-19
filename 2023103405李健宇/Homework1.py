1710acaadf9735fcbaf94b92d9f5f097ec352033
LiJianYu <95405396+UnknowLi@users.noreply.github.com>
2024-06-19 18:11:27 +0800

    Update Homework1.py

--- 2023103405李健宇/Homework1.py
+++ 2023103405李健宇/Homework1.py
@@ -3,50 +3,52 @@ import string
 import datetime
 
 
-def generate_random_data(structure, size=1):
-    def generate_value(value_type):
-        if isinstance(value_type, tuple):
-            base_type = value_type[0]
-            if base_type == 'int':
-                return random.randint(*value_type[1:])
-            elif base_type == 'str':
-                length = random.randint(*value_type[1:]) if len(value_type) > 1 else 10
-                return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
-            elif base_type == 'float':
-                return random.uniform(*value_type[1:])
-            elif base_type == 'bool':
-                return random.choice([True, False])
-            elif base_type == 'datetime':
-                start, end = value_type[1:]
-                return start + datetime.timedelta(
-                    seconds=random.randint(0, int((end - start).total_seconds()))
-                )
-            elif callable(base_type):
-                return base_type(*value_type[1:])
-        elif value_type == 'int':
-            return random.randint(0, 100)
-        elif value_type == 'str':
-            return ''.join(random.choices(string.ascii_letters + string.digits, k=10))
-        elif value_type == 'float':
-            return random.uniform(0.0, 100.0)
-        elif value_type == 'bool':
+def generate_random_value(value_type):
+    if isinstance(value_type, tuple):
+        base_type = value_type[0]
+        if base_type == 'int':
+            return random.randint(*value_type[1:])
+        elif base_type == 'str':
+            length = random.randint(*value_type[1:]) if len(value_type) > 1 else 10
+            return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+        elif base_type == 'float':
+            return random.uniform(*value_type[1:])
+        elif base_type == 'bool':
             return random.choice([True, False])
-        elif value_type == 'datetime':
-            return datetime.datetime.now()
-        else:
-            return None
+        elif base_type == 'datetime':
+            start, end = value_type[1:]
+            return start + datetime.timedelta(
+                seconds=random.randint(0, int((end - start).total_seconds()))
+            )
+        elif callable(base_type):
+            return base_type(*value_type[1:])
+    elif value_type == 'int':
+        return random.randint(0, 100)
+    elif value_type == 'str':
+        return ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+    elif value_type == 'float':
+        return random.uniform(0.0, 100.0)
+    elif value_type == 'bool':
+        return random.choice([True, False])
+    elif value_type == 'datetime':
+        return datetime.datetime.now()
+    else:
+        return None
 
-    def generate_structure(struct):
-        if isinstance(struct, dict):
-            return {k: generate_structure(v) for k, v in struct.items()}
-        elif isinstance(struct, list):
-            return [generate_structure(struct[0]) for _ in range(len(struct))]
-        elif isinstance(struct, tuple):
-            return tuple(generate_structure(struct[0]) for _ in range(len(struct)))
-        else:
-            return generate_value(struct)
 
-    return [generate_structure(structure) for _ in range(size)]
+def generate_random_structure(struct):
+    if isinstance(struct, dict):
+        return {k: generate_random_structure(v) for k, v in struct.items()}
+    elif isinstance(struct, list):
+        return [generate_random_structure(struct[0]) for _ in range(len(struct))]
+    elif isinstance(struct, tuple):
+        return tuple(generate_random_structure(struct[0]) for _ in range(len(struct)))
+    else:
+        return generate_random_value(struct)
+
+
+def generate_random_data(structure, size=1):
+    return [generate_random_structure(structure) for _ in range(size)]
 
 
 structure = {
