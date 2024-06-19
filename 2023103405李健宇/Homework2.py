3bd092ad9ef3ed18b506b57048a3c74950e43a4f
LiJianYu <95405396+UnknowLi@users.noreply.github.com>
2024-06-19 18:11:50 +0800

    Update Homework2.py

--- 2023103405李健宇/Homework2.py
+++ 2023103405李健宇/Homework2.py
@@ -5,50 +5,52 @@ import datetime
 
 class RandomDataGenerator:
     @staticmethod
-    def generate_random_data(structure, size=1):
-        def generate_value(value_type):
-            if isinstance(value_type, tuple):
-                base_type = value_type[0]
-                if base_type == 'int':
-                    return random.randint(*value_type[1:])
-                elif base_type == 'str':
-                    length = random.randint(*value_type[1:]) if len(value_type) > 1 else 10
-                    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
-                elif base_type == 'float':
-                    return random.uniform(*value_type[1:])
-                elif base_type == 'bool':
-                    return random.choice([True, False])
-                elif base_type == 'datetime':
-                    start, end = value_type[1:]
-                    return start + datetime.timedelta(
-                        seconds=random.randint(0, int((end - start).total_seconds()))
-                    )
-                elif callable(base_type):
-                    return base_type(*value_type[1:])
-            elif value_type == 'int':
-                return random.randint(0, 100)
-            elif value_type == 'str':
-                return ''.join(random.choices(string.ascii_letters + string.digits, k=10))
-            elif value_type == 'float':
-                return random.uniform(0.0, 100.0)
-            elif value_type == 'bool':
+    def generate_value(value_type):
+        if isinstance(value_type, tuple):
+            base_type = value_type[0]
+            if base_type == 'int':
+                return random.randint(*value_type[1:])
+            elif base_type == 'str':
+                length = random.randint(*value_type[1:]) if len(value_type) > 1 else 10
+                return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+            elif base_type == 'float':
+                return random.uniform(*value_type[1:])
+            elif base_type == 'bool':
                 return random.choice([True, False])
-            elif value_type == 'datetime':
-                return datetime.datetime.now()
-            else:
-                return None
+            elif base_type == 'datetime':
+                start, end = value_type[1:]
+                return start + datetime.timedelta(
+                    seconds=random.randint(0, int((end - start).total_seconds()))
+                )
+            elif callable(base_type):
+                return base_type(*value_type[1:])
+        elif value_type == 'int':
+            return random.randint(0, 100)
+        elif value_type == 'str':
+            return ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+        elif value_type == 'float':
+            return random.uniform(0.0, 100.0)
+        elif value_type == 'bool':
+            return random.choice([True, False])
+        elif value_type == 'datetime':
+            return datetime.datetime.now()
+        else:
+            return None
 
-        def generate_structure(struct):
-            if isinstance(struct, dict):
-                return {k: generate_structure(v) for k, v in struct.items()}
-            elif isinstance(struct, list):
-                return [generate_structure(struct[0]) for _ in range(len(struct))]
-            elif isinstance(struct, tuple):
-                return tuple(generate_structure(struct[0]) for _ in range(len(struct)))
-            else:
-                return generate_value(struct)
+    @staticmethod
+    def generate_structure(struct):
+        if isinstance(struct, dict):
+            return {k: RandomDataGenerator.generate_structure(v) for k, v in struct.items()}
+        elif isinstance(struct, list):
+            return [RandomDataGenerator.generate_structure(struct[0]) for _ in range(len(struct))]
+        elif isinstance(struct, tuple):
+            return tuple(RandomDataGenerator.generate_structure(struct[0]) for _ in range(len(struct)))
+        else:
+            return RandomDataGenerator.generate_value(struct)
 
-        return [generate_structure(structure) for _ in range(size)]
+    @staticmethod
+    def generate_random_data(structure, size=1):
+        return [RandomDataGenerator.generate_structure(structure) for _ in range(size)]
 
 
 structure = {
@@ -65,4 +67,3 @@ structure = {
 generator = RandomDataGenerator()
 random_students = generator.generate_random_data(structure, size=1000)
 print(random_students[:5])  # 打印前五个生成的随机学生信息
-
